de09503e5bd56fc509016f97073c4ec768cff0e9
fridrich.mrtn@gmail.com <fridrich.mrtn@gmail.com>
2022-05-05 08:52:05 +0000

    Fix windowing, clean-up the code

--- jupyters/00-rees46-data-init-bricks.py
+++ jupyters/00-rees46-data-init-bricks.py
@@ -19,41 +19,44 @@ while i<len(mnts):
         i+=1
 if i==len(mnts):
     print("Rees46 already mounted.")
-        
 
 # COMMAND ----------
+
+from pyspark.sql.types import StructType, IntegerType, DoubleType, TimestampType, StringType
+
+events_schema = StructType()\
+    .add("event_time",TimestampType(), True)\
+    .add("event_type", StringType(), True)\
+    .add("product_id", IntegerType(), True)\
+    .add("category_id", StringType(), True)\
+    .add("category_code", StringType(), True)\
+    .add("brand", StringType(), True)\
+    .add("price", DoubleType(), True)\
+    .add("user_id", IntegerType(), True)\
+    .add("user_session", StringType(), True)
 
 # load raw data
 target_files = [tf for tf in dbutils.fs.ls("dbfs:/mnt/rees46/raw")\
     if "csv.gz" in tf.path]
 for tf in target_files:
     if "events" not in locals():
-        events = spark.read.csv(tf.path, header=True)
+        events = spark.read.csv(tf.path, schema=events_schema, header=True)
     else:
-        events = events.union(spark.read.csv(tf.path, header=True))
-events.printSchema()        
+        events = events.union(spark.read.csv(tf.path, schema=events_schema, header=True))
+events.write.mode('overwrite').parquet("dbfs:/mnt/rees46/raw/_")
+events.printSchema()    
 
 # COMMAND ----------
 
-# convert dtypes, possibly move to read part
 import pyspark.sql.functions as f
-events = events.withColumn("event_time", f.to_timestamp(f.col("event_time")))\
-    .withColumn("product_id", f.col("product_id").cast("integer"))\
-    .withColumn("category_id", f.col("category_id"))\
-    .withColumn("price", f.col("price").cast("double"))\
-    .withColumn("user_id", f.col("user_id").cast("integer"))
-events.printSchema()
-
+events = spark.read.parquet("dbfs:/mnt/rees46/raw/_")\
+    .repartition(200).persist()#sample(fraction=1.0, seed=202205)
+events.rdd.getNumPartitions()
 
 # COMMAND ----------
 
-events.write.parquet("dbfs:/mnt/rees46/temp")
-events = spark.read.parquet("dbfs:/mnt/rees46/temp")
-events = events.persist()
-events.show() # instantiate
-
-# COMMAND ----------
-
+# drop na user sessions
+events = events.na.drop(subset=["user_session"]) # drop rswous
 # construct id cols
 def add_ind(df, colmn):
     from pyspark.sql.functions import dense_rank
@@ -64,11 +67,7 @@ def add_ind(df, colmn):
 col_to_map = ["event_type", "product_id", "category_id", "user_id", "user_session"]
 for c in col_to_map:
     events = add_ind(events, c)
-events = events.na.drop(subset=["user_session"]) # remove rows wo user_session   
-#events.show(3)    
-
-# COMMAND ----------
-
+# rename
 events = events.select("event_time", "event_type", "category_code", "brand", "price",
      "new_event_type", "new_product_id", "new_category_id", "new_user_id",
          "new_user_session")\
@@ -77,34 +76,45 @@ events = events.select("event_time", "event_type", "category_code", "brand", "pr
     .withColumnRenamed("new_category_id", "category_id")\
     .withColumnRenamed("new_user_id", "user_id")\
     .withColumnRenamed("new_user_session", "user_session_id")
-events = events.persist()    
-events.show(3)  
+
+#from pyspark.storagelevel import StorageLevel as sl
+events = events.persist()
+events.show(3)    
 
 # COMMAND ----------
 
 # pushdown tables
 def save_tables(df, location):
-    # carve-out the tabs
-    event_types = df.select(f.col("event_type_id"),
-        f.col("event_type").alias("event_type_name")).dropDuplicates()
-    # carve-out remaining tabs based on the last observed vals
-    products = df.groupBy("product_id").agg(f.max("event_time").alias("event_time"))\
-        .join(df, on=["product_id", "event_time"], how="inner")\
-            .select("product_id", "category_id", "brand").dropDuplicates()
-    categories = df.groupBy("category_id").agg(f.max("event_time").alias("event_time"))\
-        .join(df, on=["category_id", "event_time"], how="inner")\
-            .select("category_id", "category_code").dropDuplicates()
-    events = df.select(["event_time", "user_id", "product_id", "event_type_id", "price", "user_session_id"])
-    # push down
-    events.write.parquet(location+"events", mode="overwrite")
-    products.write.parquet(location+"products", mode="overwrite")
-    categories.write.parquet(location+"categories", mode="overwrite")
-    event_types.write.parquet(location+"event_types", mode="overwrite")
+    from pyspark.sql.window import Window
+    import pyspark.sql.functions as f
+    # event types
+    df.select(f.col("event_type_id"),
+        f.col("event_type").alias("event_type_name")).dropDuplicates()\
+        .write.mode("overwrite").parquet(location+"event_types")
+    # products
+    wp = Window.partitionBy("product_id").orderBy("event_time")\
+        .rowsBetween(Window.unboundedPreceding, Window.unboundedFollowing)
+    df.withColumn("last_brand", f.last("brand", True).over(wp))\
+        .withColumn("last_category_id", f.last("category_id", True).over(wp))\
+        .select(f.col("product_id"), f.col("last_category_id").alias("category_id"),
+               f.col("last_brand").alias("brand")).dropDuplicates()\
+        .write.mode("overwrite").parquet(location+"products")
+    # categories
+    wc = Window.partitionBy("category_id").orderBy("event_time")\
+        .rowsBetween(Window.unboundedPreceding, Window.unboundedFollowing)
+    df.withColumn("last_category_code", f.last("category_code", True).over(wc))\
+        .select(f.col("category_id"), f.col("last_category_code").alias("category_code")\
+                   ).dropDuplicates()\
+        .write.mode("overwrite").parquet(location+"categories")
+    # events
+    df.select(["event_time", "user_id", "product_id", "event_type_id", "price",
+               "user_session_id"])\
+        .write.mode("overwrite").parquet(location+"events")
 
 DATA_OUT = "dbfs:/mnt/rees46/raw/concatenated/"
 events.write.parquet(DATA_OUT+"_", mode="overwrite")
 save_tables(events, DATA_OUT+"full/")
-save_tables(events.sample(fraction=.05, seed=202205), DATA_OUT+"sample/") 
+save_tables(events.sample(fraction=.01, seed=202205), DATA_OUT+"sample/") 
 
 # COMMAND ----------
 
@@ -112,3 +122,7 @@ save_tables(events.sample(fraction=.05, seed=202205), DATA_OUT+"sample/")
 #dbutils.fs.rm("dbfs:/mnt/rees46/temp/events", True)
 #dbutils.fs.rm("dbfs:/mnt/rees46/temp", True)
 #del events
+
+# COMMAND ----------
+
+
