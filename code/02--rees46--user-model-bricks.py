92f1952c43dca1f32a15e5a7768b97962eab3979
fridrich.mrtn@gmail.com <fridrich.mrtn@gmail.com>
2022-05-08 11:30:12 +0000

    Add stats & lags

--- code/02--rees46--user-model-bricks.py
+++ code/02--rees46--user-model-bricks.py
@@ -40,9 +40,6 @@ sessions = events.groupBy("user_session_id", "user_id").agg(
         f.min("event_time").alias("start"), f.max("event_time").alias("end"),
         ((f.max("event_time")-f.min("event_time")).cast("long")/60).alias("length"),
         # date-time components
-        # try 
-    
-        #
         f.make_date(f.year(f.min("event_time")),f.month(f.min("event_time")),f.lit(1)).alias("start_monthgroup"),
         f.year(f.min("event_time")).alias("start_year"), f.dayofyear(f.min("event_time")).alias("start_yearday"), 
         f.month(f.min("event_time")).alias("start_month"), f.dayofmonth(f.min("event_time")).alias("start_monthday"),
@@ -93,11 +90,77 @@ sessions.printSchema()
 # COMMAND ----------
 
 # MAGIC %md
+# MAGIC ### Aggregate users
+
+# COMMAND ----------
+
+# statistics
+def cv(colname):
+    # coeficient of variation
+    import pyspark.sql.functions as f
+    return (f.mean(colname)/f.stddev_samp(colname))
+excl_cols = set(["user_session_id", "user_id", "start", "end", "start_monthgroup"])
+stat_cols = [c for c in sessions.columns if c not in excl_cols]
+stat_funcs = [f.mean, f.sum, f.min, f.max, f.stddev_samp, cv] # extend?
+stat_exp = [f(c).alias(c+"_"+str(f.__name__).split("_")[0])for f in stat_funcs for c in stat_cols]
+# hand-crafted interactions
+int_exp = [(f.max("session_number")/f.max("session_recency")).alias("session_count_daily_ratio"),
+    (f.sum("click_count")/f.max("session_number")).alias("click_count_ratio"),
+    (f.sum("purchase_count")/f.max("session_number")).alias("transaction_count_ratio")]
+agg_exp = stat_exp + int_exp
+users = sessions.groupBy("user_id").agg(*agg_exp)
+users.filter(f.col("user_id")==3960141).toPandas()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Lagged features
+
+# COMMAND ----------
+
+user_month = sessions.select("user_id").distinct().crossJoin(sessions.select("start_monthgroup").distinct())
+user_month_groups = sessions.groupBy("user_id", "start_monthgroup").agg(
+    (f.count("user_session_id")).alias("session_count"),
+    (f.sum("haspurchase")).alias("purchase_count"),
+    (f.sum("purchase_revenue")).alias("purchase_revenue"))
+user_month_groups = user_month.join(user_month_groups,
+    on=["user_id", "start_monthgroup"], how="left")
+
+def add_lags(df, colnames, lags=[0,1,2,3], ma=3):
+    import pyspark.sql.functions as f
+    from pyspark.sql.window import Window
+    uw = Window.partitionBy("user_id").orderBy("start_monthgroup")
+    uwr = Window.partitionBy("user_id").orderBy(f.col("start_monthgroup")).rowsBetween(-ma,0)
+    # lags
+    for c in colnames:
+        for l in lags:
+            df = df.withColumn(c+"_month_lag"+str(l), f.lag(c,l).over(uw)) # lags
+        df = df.withColumn(c+"_month_ma"+str(ma), f.mean(f.col("session_count")).over(uwr)) # moving avg
+        # possibly add diff, and a few other interactions
+    return df
+
+user_month_lags = add_lags(user_month_groups,
+    ["session_count", "purchase_count", "purchase_revenue"])
+
+last_monthgroup = user_month_lags.agg(f.max("start_monthgroup").alias("smg"))\
+    .collect()[0].__getitem__("smg")
+cols = [c for c in user_month_lags.columns if ("_ma" in c) or ("_lag" in c) or ("user_id" in c)]
+user_month_lags = user_month_lags.where(f.col("start_monthgroup")==last_monthgroup).select(*cols)
+users = users.join(user_month_lags, on=["user_id"], how="inner")    
+users.filter(f.col("user_id")==3960141).toPandas()
+del user_month_groups, user_month_lags
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %md
 # MAGIC ### WHAT TO DO TOMORROW
-# MAGIC * expand the cols
-# MAGIC * add handcrafted interactions
-# MAGIC * add grouping, incl full lhs
-# MAGIC * add lags for revenue, sessions, transactions, consider smoothing
+# MAGIC * expand the cols - DONE
+# MAGIC * add handcrafted interactions - DONE
+# MAGIC * add lags for revenue, sessions, transactions, consider smoothing + lhs for grouping - DONE
 # MAGIC 
 # MAGIC ### WHAT TO DO THE DAY AFTER TOMORROW
 # MAGIC * pick preference representation
@@ -107,60 +170,6 @@ sessions.printSchema()
 # MAGIC * TARGET VECTORS
 # MAGIC * GENERAL FILTERS
 # MAGIC * ML PIPE
-
-# COMMAND ----------
-
-# test the aggs
-cols = ["time_to_view_revenue", "time_to_view", "view_count"]
-agg_funcs = [f.mean, f.sum, f.min, f.max, f.stddev_samp]
-agg_exp = [f(c).alias(c+"_"+str(f.__name__).split("_")[0])for f in agg_funcs for c in cols]
-sessions.groupBy("user_id").agg(*agg_exp).show()
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC ### Aggregate users
-
-# COMMAND ----------
-
-# get last session date
-
-
-wr = Window().partitionBy("user_id").orderBy("session_number")
-
-sessions.groupBy("user_id").agg(
-    # recency
-    (f.min("session_recency")).alias("session_recency"),
-    f.mean("inter_session_time").alias("session_recency_avg"),
-    f.stddev_samp("inter_session_time").alias("session_recency_sd"),
-    (f.stddev_samp("inter_session_time")/f.mean("inter_session_time")).alias("session_recency_cv"),
-    (f.max("session_recency")).alias("user_maturity"),
-    # frequency
-    f.max("session_number").alias("session_count"),
-    (f.max("session_number")/f.max("session_recency")).alias("session_count_daily_ratio"),
-    f.sum("click_count").alias("click_count"),
-    (f.sum("click_count")/f.max("session_number")).alias("click_count_ratio"),
-    f.sum(f.when(f.col("purchase_count")>0,1).otherwise(0)).alias("transaction_count"),
-    (f.sum(f.when(f.col("purchase_count")>0,1).otherwise(0))/f.max("session_number")).alias("transaction_count_ratio"),
-    # monetary
-    (f.sum("view_revenue")).alias("session_viewed_revenue"),
-    (f.sum("cart_revenue")).alias("session_cart_revenue"),
-    f.sum("purchase_revenue").alias("session_purchase_revenue"),
-    # others
-        # date-time NOTE: push part of the calculations to the sessions level
-    (f.mean(f.hour(f.col("session_start")))).alias("session_hour_avg"),
-    (f.stddev_samp(f.hour(f.col("session_start")))).alias("session_hour_sd"),
-    (f.mean(f.dayofweek(f.col("session_start")))).alias("session_wday_avg"),
-    (f.stddev_samp(f.dayofweek(f.col("session_start")))).alias("session_wday_sd"),
-    f.mean(f.when(f.dayofweek(f.col("session_start"))==1,1)\
-           .when(f.dayofweek(f.col("session_start"))==7,1).otherwise(0)\
-          ).alias("session_weekend_ratio"),
-        # intermeasures
-    
-        # session length
-    f.mean("session_length").alias("session_length_avg")
-    
-).select("user_id", "session_weekend_ratio").show()
 
 # COMMAND ----------
 
