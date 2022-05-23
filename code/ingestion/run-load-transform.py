156c6028d4c06ebbf1523a806614ae13155c91c3
fridrich.mrtn@gmail.com <fridrich.mrtn@gmail.com>
2022-05-23 12:44:55 +0000

    Run on full rees46

--- code/ingestion/run-load-transform.py
+++ code/ingestion/run-load-transform.py
@@ -3,12 +3,10 @@
 
 # COMMAND ----------
 
-
-
 #
 ##
 ### REES46
 
-data_path = "dbfs:/mnt/rees46/"
-events = construct_events(data_path+"raw", "rees46")
-save_events(events, data_path+"concatenated/test_run_events")
+data_path = "dbfs:/mnt/rees46/raw/"
+events = construct_events(data_path, "rees46")
+save_events(events, data_path+"concatenated/events/")
