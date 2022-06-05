9ccc6265cafe2e1927cb2487ca0de7518e674f96
fridrich.mrtn@gmail.com <fridrich.mrtn@gmail.com>
2022-06-05 19:14:24 +0000

    Add MLP class

--- code/modeling/utils.py
+++ code/modeling/utils.py
@@ -51,3 +51,40 @@ class HierarchicalFeatureSelector(SelectorMixin, BaseEstimator):
         return self
     def _get_support_mask(self):
         return np.array([c in set(self.best_.feature) for c in self.in_features_])
+      
+# MLPC
+from tensorflow import keras
+from scikeras.wrappers import KerasClassifier
+
+class MLPClassifier(KerasClassifier):
+
+    def __init__(
+        self,
+        n_layers=1,
+        layer_size=8,
+        activation="relu",
+        optimizer="adam",
+        optimizer__learning_rate=10**-3,
+        epochs=50,
+        verbose=0,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+        self.n_layers = n_layers
+        self.layer_size = layer_size
+        self.activation = activation
+        self.optimizer = optimizer
+        self.epochs = epochs
+        self.verbose = verbose
+
+    def _keras_build_fn(self, compile_kwargs):
+        model = keras.Sequential()
+        inp = keras.layers.Input(shape=(self.n_features_in_))
+        model.add(inp)
+        for i in range(self.n_layers):
+            model.add(keras.layers.Dense(self.layer_size,
+                activation=self.activation))
+            model.add(keras.layers.BatchNormalization())
+        model.add(keras.layers.Dense(1, activation="sigmoid"))
+        model.compile(loss="binary_crossentropy", optimizer=compile_kwargs["optimizer"])
+        return model
