beb877f29824e3dae7908011d6c68edde1df8f10
fridrich.mrtn@gmail.com <fridrich.mrtn@gmail.com>
2022-06-07 09:55:38 +0000

    Adjust formatting

--- code/modeling/step-spaces.py
+++ code/modeling/step-spaces.py
@@ -14,20 +14,24 @@ from imblearn.pipeline import Pipeline
 from sklearn.cluster import AgglomerativeClustering
 
 class DataFrameTransposer(BaseEstimator, TransformerMixin):
+  
     def fit(self, X, y=None):
         return self
     def transform(self, X, y=None):
         return X.copy().T
     
 class HierarchicalFeatureSelector(SelectorMixin, BaseEstimator):
+  
     def __init__(self, n_features=10, alpha=0.001):
         self.n_features = n_features
         self.alpha = alpha
+        
     def get_cluster_assignments_(self, data):
         data = data.loc[:,self.results_.feature.values]
         pipe = Pipeline([("rotate", DataFrameTransposer()),
             ("cluster", AgglomerativeClustering(n_clusters=self.n_features))])
         return pipe.fit_predict(data)
+    
     def get_correlations_(self, X, y):
         tf_corr = [pearsonr(y, X[c]) for c in X.columns]
         correlations = pd.DataFrame(tf_corr, index=X.columns).reset_index()
@@ -35,6 +39,7 @@ class HierarchicalFeatureSelector(SelectorMixin, BaseEstimator):
         correlations["abs_r"] = correlations.r.abs()
         correlations["sf"] = correlations.p<=self.alpha/X.shape[1]
         return correlations
+    
     def fit(self, X, y):
         X = pd.DataFrame(X)
         y = pd.Series(y)
@@ -49,6 +54,7 @@ class HierarchicalFeatureSelector(SelectorMixin, BaseEstimator):
                     as_index=False).abs_r.max(), on=["cluster", "abs_r"])\
                         .drop_duplicates(["cluster", "abs_r"]).dropna()
         return self
+    
     def _get_support_mask(self):
         return np.array([c in set(self.best_.feature) for c in self.in_features_])
       
@@ -58,25 +64,18 @@ from scikeras.wrappers import KerasClassifier
 
 class MLPClassifier(KerasClassifier):
 
-    def __init__(
-        self,
-        n_layers=1,
-        layer_size=8,
-        activation="relu",
-        optimizer="adam",
+    def __init__(self, n_layers=1, layer_size=8,
+        activation="relu", optimizer="adam",
         optimizer__learning_rate=10**-3,
-        epochs=50,
-        verbose=0,
-        **kwargs,
-    ):
-        super().__init__(**kwargs)
-        self.n_layers = n_layers
-        self.layer_size = layer_size
-        self.activation = activation
-        self.optimizer = optimizer
-        self.epochs = epochs
-        self.verbose = verbose
-
+        epochs=50, verbose=0, **kwargs,):
+            super().__init__(**kwargs)
+            self.n_layers = n_layers
+            self.layer_size = layer_size
+            self.activation = activation
+            self.optimizer = optimizer
+            self.epochs = epochs
+            self.verbose = verbose
+        
     def _keras_build_fn(self, compile_kwargs):
         model = keras.Sequential()
         inp = keras.layers.Input(shape=(self.n_features_in_))
