55566840d561ee1025e661b8e25eafea1f4a11ad
fridrich.mrtn@gmail.com <fridrich.mrtn@gmail.com>
2022-06-09 12:15:51 +0000

    Work in progress

--- code/modeling/workhorse.py
+++ code/modeling/workhorse.py
@@ -23,13 +23,15 @@ def _get_data(dataset_name, week_step):
         .where(f.col("week_step")==week_step).toPandas()
     out_cols = ["user_id", "target_event", "target_revenue", "week_step"]
     feat_cols = [c for c in train.columns if c not in set(out_cols)]
-    return {"train":
-                {"X":_optimize_numeric_dtypes(train.loc[:,feat_cols]),
-                 "y":train["target_event"]},
-            "test":
-                {"X":_optimize_numeric_dtypes(test.loc[:,feat_cols]),
-                 "y":test["target_event"]},
-            "name":f"{dataset_name}_{week_step}"}
+    return {
+      "train":
+            {"X":_optimize_numeric_dtypes(train.loc[:,feat_cols]),
+             "y":train["target_event"]},
+      "test":
+            {"X":_optimize_numeric_dtypes(test.loc[:,feat_cols]),
+             "y":test["target_event"]},
+      "name":
+            f"{dataset_name}_{week_step}"}
 
 def glue_pipeline(pipe, data, refit=True):
     import mlflow
@@ -67,7 +69,7 @@ def glue_pipeline(pipe, data, refit=True):
     results = pd.DataFrame([dict(_get_performance(
         calibrated_pipeline, v["X"], v["y"]),
             **{"type":k,"week_step":week_step,"pipe":pipe_name})
-                for k,v in data.items() if k in ["train", "test"]])
+                for k,v in data.items() if k in ["train", "test"]]) # NOT NICE
     spark.createDataFrame(results)\
         .write.format("delta").mode("append")\
             .saveAsTable(f"churndb.{dataset_name}_performance_evaluation")
@@ -75,29 +77,40 @@ def glue_pipeline(pipe, data, refit=True):
 
 # COMMAND ----------
 
-# NOTE: streamline this, push pipe spaces to optimization directly
-# NOTE: 
+# try it with much smaller dataset
+from sklearn.datasets import load_breast_cancer
+from imblearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.over_sampling import RandomOverSampler
 
-import datetime
 import mlflow
 
-pipe_name = "dt"
-data = _get_data("rees46", 2)
-pipe = get_pipeline(pipe_name)
-steps = pipe["steps"]
-space = pipe["space"]
 
-b_data = sc.broadcast(data)
-b_pipe = sc.broadcast(pipe)
+steps = get_pipeline("dt")["steps"]
+space = get_pipeline("dt")["space"]
 
+#data = _get_data("rees46", 5)
 
-
-# COMMAND ----------
+data = load_breast_cancer(as_frame=True)
+X_train, X_test, y_train, y_test = train_test_split(data["data"],data["target"], test_size=.4)
 
 with mlflow.start_run() as run:  
     results = _optimize_pipeline(
-        b_data.value["train"]["X"], b_data.value["train"]["y"],
-            b_pipe.value["steps"], b_pipe.value["space"])
+        X_train, y_train,
+            steps, space)
+
+# COMMAND ----------
+
+
+    
+
+# COMMAND ----------
+
+# ASYNC HYPEROPT AND FIT/CALIBRATION, CLEAN CODE
+# REIMPLEMENT CALIBRATION
+# TRY WET RUN ON 2 WEEKS AND CLFS
 
 # COMMAND ----------
 
@@ -133,191 +146,57 @@ for week_step in range(2,4):
 
 # COMMAND ----------
 
-import numpy as np
- 
-from sklearn.datasets import fetch_california_housing
-from sklearn.model_selection import cross_val_score
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
- 
-from hyperopt import fmin, tpe, hp, SparkTrials, STATUS_OK, Trials
- 
-import mlflow
-X, y = fetch_california_housing(return_X_y=True)
 
-from sklearn.preprocessing import StandardScaler
- 
-scaler = StandardScaler()
-X = scaler.fit_transform(X)
-y_discrete = np.where(y < np.median(y), 0, 1)
-
-
-def objective(params):
-    classifier_type = params['type']
-    del params['type']
-    if classifier_type == 'svm':
-        clf = SVC(**params)
-    elif classifier_type == 'rf':
-        clf = RandomForestClassifier(**params)
-    elif classifier_type == 'logreg':
-        clf = LogisticRegression(**params)
-    else:
-        return 0
-    accuracy = cross_val_score(clf, X, y_discrete).mean()
-    
-    
-    # Because fmin() tries to minimize the objective, this function must return the negative accuracy. 
-    return {'loss': -accuracy, 'status': STATUS_OK}
-
-search_space = hp.choice('classifier_type', [
-    {
-        'type': 'svm',
-        'C': hp.lognormal('SVM_C', 0, 1.0),
-        'kernel': hp.choice('kernel', ['linear', 'rbf'])
-    },
-    {
-        'type': 'rf',
-        'max_depth': hp.randint('max_depth', 2, 5),
-        'criterion': hp.choice('criterion', ['gini', 'entropy'])
-    },
-    {
-        'type': 'logreg',
-        'C': hp.lognormal('LR_C', 0, 1.0),
-        'solver': hp.choice('solver', ['liblinear', 'lbfgs'])
-    },
-])
-
-algo=tpe.suggest
-spark_trials = SparkTrials(parallelism=4)
-#spark_trials=Trials()
-data_pipe_name = "lulw"
-exp_name = f"{data_pipe_name}_hyperopt"
-exp_id = _get_exp_id(f"/Shared/dev/{exp_name}")
-mlflow.set_experiment(experiment_id=exp_id)
-with mlflow.start_run():
-    best_result = fmin(
-        fn=objective, 
-        space=search_space,
-        algo=algo,
-        max_evals=32,
-        trials=spark_trials)
-    mlflow.log_params(best_result)
 
 # COMMAND ----------
 
-# Databricks notebook source
-# MAGIC 
-# MAGIC %md
-# MAGIC # Hyperopt
-# MAGIC 
-# MAGIC The [Hyperopt library](https://github.com/hyperopt/hyperopt) allows for parallel hyperparameter tuning using either random search or Tree of Parzen Estimators (TPE). With MLflow, we can record the hyperparameters and corresponding metrics for each hyperparameter combination. You can read more on [SparkTrials w/ Hyperopt](https://github.com/hyperopt/hyperopt/blob/master/docs/templates/scaleout/spark.md).
-# MAGIC 
-# MAGIC For this example, we will parallelize the hyperparameter search for a tf.keras model with the california housing dataset.
+Scaler()
 
 # COMMAND ----------
 
-from sklearn.datasets import fetch_california_housing
+from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 
-cal_housing = fetch_california_housing()
 
-# split 80/20 train-test
-X_train, X_test, y_train, y_test = train_test_split(cal_housing.data,
-                                                    cal_housing.target,
-                                                    test_size=0.2,
-                                                    random_state=1)
-# Feature-wise standardization
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+from sklearn.preprocessing import StandardScaler, QuantileTransformer
+from sklearn.linear_model import LogisticRegression
+from imblearn.pipeline import Pipeline
 
-# COMMAND ----------
+from hyperopt import hp, tpe, fmin, SparkTrials, Trials, STATUS_OK, space_eval
+from functools import partial
+from imblearn.base import FunctionSampler
 
-# MAGIC %md-sandbox
-# MAGIC 
-# MAGIC ## Keras Model
-# MAGIC 
-# MAGIC We will define our NN in Keras and use the hyperparameters given by HyperOpt.
-# MAGIC 
-# MAGIC <img alt="Side Note" title="Side Note" style="vertical-align: text-bottom; position: relative; height:1.75em; top:0.05em; transform:rotate(15deg)" src="https://files.training.databricks.com/static/images/icon-note.webp"/> We need to import `tensorflow` within the function due to a pickling issue.  <a href="https://docs.databricks.com/applications/deep-learning/single-node-training/tensorflow.html#tensorflow-2-known-issues" target="_blank">See known issues here.</a>
 
-# COMMAND ----------
 
-import tensorflow as tf
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.models import Sequential
-import mlflow
-import mlflow.keras
-tf.random.set_seed(42)
+pipe = Pipeline([("sa", Sampler(sampling_strategy="under_sampling")),
+    ("sc", Scaler(scaling_strategy="robust")),
+    ("lr", LogisticRegression())])
 
-def create_model(hpo):
-    model = Sequential()
-    model.add(Dense(int(hpo["dense_l1"]), input_dim=8, activation="relu"))
-    model.add(Dense(int(hpo["dense_l2"]), activation="relu"))
-    model.add(Dense(1, activation="linear"))
-    return model
+data = load_breast_cancer(as_frame=True)
+X_train, X_test, y_train, y_test = train_test_split(data["data"],data["target"], test_size=.4)
 
-# COMMAND ----------
-
-from hyperopt import fmin, hp, tpe, STATUS_OK, SparkTrials
-
-def runNN(hpo):
-  # Need to include the TF import due to serialization issues
-    import tensorflow as tf
-  
-    model = create_model(hpo)
-
-    # Select Optimizer
-    optimizer_call = getattr(tf.keras.optimizers, hpo["optimizer"])
-    optimizer = optimizer_call(learning_rate=hpo["learning_rate"])
-
-  # Compile model
-    model.compile(loss="mse",
-                optimizer=optimizer,
-                metrics=["mse"])
-
-    history = model.fit(X_train, y_train, validation_split=.2, epochs=10, verbose=2)
-
-    # Evaluate our model
-    score = model.evaluate(X_test, y_test, verbose=0)
-    obj_metric = score[0]  
+def run_lr(params, pipe): 
+    #alpha = params["alpha"]
+    pipe.set_params(**params)
+    pipe.fit(X_train,y_train)
+    obj_metric = pipe.score(X_test, y_test)
     return {"loss": obj_metric, "status": STATUS_OK}
 
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC ### Setup hyperparameter space and training
-# MAGIC 
-# MAGIC We need to create a search space for HyperOpt and set up SparkTrials to allow HyperOpt to run in parallel using Spark worker nodes. We can also start a MLflow run to automatically track the results of HyperOpt's tuning trials.
-
-# COMMAND ----------
-
 space = {
-  "dense_l1": hp.quniform("dense_l1", 10, 30, 1),
-  "dense_l2": hp.quniform("dense_l2", 10, 30, 1),
-  "learning_rate": hp.loguniform("learning_rate", -5, 0),
-  "optimizer": hp.choice("optimizer", ["Adadelta", "Adam"])
- }
+  "lr__C": hp.uniform("lr__C", 10**-3, 10),
+  "sa__sampling_strategy": hp.choice("sa__sampling_strategy",["under_sampling","over_sampling"]),
+  "sc__scaling_strategy":hp.choice("sa__scaling_strategy",["power","quantile", "robust"])
+}
 
-spark_trials = SparkTrials(parallelism=4)
-
+spark_trials =  SparkTrials(parallelism=8)
+#spark_trials = Trials()
 with mlflow.start_run():
-    best_hyperparam = fmin(fn=runNN, 
-                         space=space, 
-                         algo=tpe.suggest, 
-                         max_evals=16, 
-                         trials=spark_trials)
-
-best_hyperparam
+    best_hyperparam = fmin(fn=partial(run_lr, pipe=pipe), 
+    space=space, 
+    algo=tpe.suggest, 
+    max_evals=16, 
+    trials=spark_trials)
 
 # COMMAND ----------
 
-# MAGIC %md
-# MAGIC To view the MLflow experiment associated with the notebook, click the Runs icon in the notebook context bar on the upper right. There, you can view all runs. You can also bring up the full MLflow UI by clicking the button on the upper right that reads View Experiment UI when you hover over it.
-# MAGIC 
-# MAGIC To understand the effect of tuning a hyperparameter:
-# MAGIC 
-# MAGIC 0. Select the resulting runs and click Compare.
-# MAGIC 0. In the Scatter Plot, select a hyperparameter for the X-axis and loss for the Y-axis.
+
