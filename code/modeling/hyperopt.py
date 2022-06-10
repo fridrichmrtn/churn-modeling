de0405351c302ce33c2e12d870024b8ce1db3cbb
fridrich.mrtn@gmail.com <fridrich.mrtn@gmail.com>
2022-06-10 07:44:10 +0000

    Remove mlflow from utility func

--- code/modeling/hyperopt.py
+++ code/modeling/hyperopt.py
@@ -3,13 +3,12 @@
 
 # COMMAND ----------
 
-#
-##
-### PIPELINE HYPEROPT
-
 hyperopt_config = {
     "max_evals":5,
     "seed":20220602}
+#
+##
+### PIPELINE HYPEROPT
 
 def _get_exp_id(exp_path):
     import mlflow
@@ -45,21 +44,21 @@ def _get_performance(model, X, y):
 def _evaluate_pipeline(params, pipe, X, y, seed):
     from hyperopt import STATUS_OK
     from sklearn.model_selection import train_test_split
-    import mlflow
+    #import mlflow
     
     data_dict = _train_test_dict(X, y, .4, seed)
-    with mlflow.start_run(nested=True) as run:
-        mlflow.log_params(params)
-        pipe.set_params(**params)
-        pipe.fit(data_dict["train"]["X"], data_dict["train"]["y"])
-        metrics = {n+"_"+m:v for n, data in data_dict.items()
-            for m,v in _get_performance(pipe, data["X"], data["y"]).items()}
-        mlflow.log_metrics(metrics)
+    #with mlflow.start_run(nested=True) as run:
+    #mlflow.log_params(params)
+    pipe.set_params(**params)
+    pipe.fit(data_dict["train"]["X"], data_dict["train"]["y"])
+    metrics = {n+"_"+m:v for n, data in data_dict.items()
+        for m,v in _get_performance(pipe, data["X"], data["y"]).items()}
+    #mlflow.log_metrics(metrics)
     return {"loss":-metrics["test_f1_score"], "status":STATUS_OK}
 
 
 def _optimize_pipeline(X, y, pipe, space):
-    import mlflow
+    #import mlflow
     from hyperopt import fmin, tpe, space_eval, SparkTrials, Trials
     from functools import partial
     
@@ -68,6 +67,6 @@ def _optimize_pipeline(X, y, pipe, space):
     space_optimized = fmin(
         fn=partial(_evaluate_pipeline,
             pipe=pipe, X=X, y=y, seed=seed),
-        trials=SparkTrials(parallelism=5),
+        trials=SparkTrials(parallelism=4),
         space=space, max_evals=max_evals, algo=tpe.suggest)
     return pipe.set_params(**space_eval(space, space_optimized))
