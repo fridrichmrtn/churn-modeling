5a19120dc82d36c9846bb4265bc80d0309389718
fridrich.mrtn@gmail.com <fridrich.mrtn@gmail.com>
2022-07-02 12:46:53 +0000

    Reorganize code

--- code/modeling/profit-evaluation.py
+++ code/modeling/profit-evaluation.py
@@ -1,6 +1,8 @@
 # Databricks notebook source
 # MAGIC %md
 # MAGIC 
+# MAGIC # NOTE: MOVE THIS TO PROFIT DEV IPYNB
+# MAGIC 
 # MAGIC ### Profit estimation
 # MAGIC 
 # MAGIC 
@@ -36,209 +38,6 @@
 
 # COMMAND ----------
 
-def get_purchases(dataset_name):
-    import pyspark.sql.functions as f
-    import pyspark.pandas as ps
-    
-    data_path = f"dbfs:/mnt/{dataset_name}/delta/events"
-    events = spark.read.format("delta").load(data_path)
-    purchases = events.where(f.col("event_type_name")=="purchase")\
-        .withColumn("date", f.to_date("event_time"))\
-        .groupBy(["product_id", "date"])\
-            .agg(f.sum("revenue").alias("revenue"))
-    products = purchases.select("product_id").distinct()
-    dates = purchases.select("date").distinct()    
-    return {"purchases":ps.DataFrame(purchases),
-            "products":ps.DataFrame(products), "dates":ps.DataFrame(dates)}
-
-def randomize_m(products, dates, mu, scale, seed):
-    import numpy as np
-    import pyspark.pandas as ps
-    
-    n_products = len(products) 
-    n_dates = len(dates)
-    scale_diff = scale/np.sqrt(n_dates)
-    # product-date margin
-    np.random.seed(int(seed))
-    product_baseline = np.random.normal(loc=mu, scale=scale, size=(n_products,1))
-    product_diffs = np.random.normal(loc=0, scale=scale_diff, size=(n_products, n_dates))
-    concat_diffs = np.concatenate([product_baseline, product_diffs], axis=1)
-    margins = ps.DataFrame(np.cumsum(concat_diffs, axis=1)[:,1:],
-        columns=dates, index=products)
-    margins = ps.melt(margins.reset_index().rename(columns={"index":"product_id"}),
-        id_vars=["product_id"], value_name="margin", var_name="date")
-    return margins.to_spark()
-
-def get_avgm(purchases, margins):
-    import pyspark.sql.functions as f
-    
-    avg_margin = purchases.join(margins, on=["product_id", "date"], how="inner")\
-        .agg((f.sum(f.col("revenue")*f.col("margin"))/f.sum("revenue")))\
-            .toPandas().values[0]
-    return float(avg_margin)
-    
-def construct_space(seed):
-    import numpy as np
-    from itertools import product
-    import pyspark.pandas as ps
-
-    np.random.seed(int(seed))
-    loc = np.linspace(0.05, 0.35, num=5)
-    scale = np.linspace(0.01, 0.15, num=5)
-    seed = np.random.randint(low=0, high=2**16, size=5)
-    space = ps.DataFrame(list(product(loc, scale, seed)),
-        columns=["loc", "scale", "seed"])
-    return space
-
-def apply_avgm(row, purchases):
-    margins = randomize_m(
-        purchases["products"], purchases["dates"],
-        mu=row["loc"], scale=row["scale"], seed=row["seed"])
-    return get_avgm(purchases["purchases"], margins)
-
-def plot_space(space):
-    import numpy as np
-    from scipy.interpolate import griddata
-    import matplotlib.pyplot as plt
-    
-    X, Y, Z = space["loc"], space["scale"], space["margin"]
-    plotx, ploty = np.meshgrid(
-        np.linspace(np.min(X),np.max(X),25),
-        np.linspace(np.min(Y),np.max(Y),25))
-    plotz = griddata((X,Y), Z, (plotx, ploty), method="cubic")
-
-    fig = plt.figure(figsize=(15,10))
-    ax = fig.add_subplot(111, projection="3d")
-    #ax.view_init(elev=50, azim=30)
-    ax.plot_surface(plotx, ploty, plotz, cmap="viridis")
-    ax.set_xlabel("$\mu_0$")
-    ax.set_ylabel("$\sigma_0$")
-    ax.set_zlabel("expected margin")
-    return ax
-
-# COMMAND ----------
-
-import pyspark.pandas as ps
-ps.set_option("compute.ops_on_diff_frames", True)
-purchases = get_purchases("rees46")
-space = construct_space(0)
-space["margin"] = space.apply(apply_avgm, axis=1, purchases=purchases)
-
-# COMMAND ----------
-
-plot_space(space.to_pandas());`
-
-# COMMAND ----------
-
-purchases = get_purchases("rees46")
-products = purchases["products"]
-dates = purchases["dates"]
-
-# COMMAND ----------
-
-import numpy as np
-
-loc=0.15
-scale=0.15
-seed=1
-
-n_products = products.shape[0]
-n_dates = dates.shape[0]
-scale_diff = scale/np.sqrt(n_dates)
-
-# COMMAND ----------
-
-import numpy as np
-import pyspark.pandas as ps
-
-n_products = len(products) 
-n_dates = len(dates)
-scale_diff = scale/np.sqrt(n_dates)
-# product-date margin
-np.random.seed(int(seed))
-product_baseline = np.random.normal(loc=loc, scale=scale, size=(n_products,1))
-product_diffs = np.random.normal(loc=0, scale=scale_diff, size=(n_products, n_dates))
-concat_diffs = np.concatenate([product_baseline, product_diffs], axis=1)
-margins = ps.DataFrame(np.cumsum(concat_diffs, axis=1)[:,1:],
-    columns=dates, index=products)
-margins = ps.melt(margins.reset_index().rename(columns={"index":"product_id"}),
-    id_vars=["product_id"], value_name="margin", var_name="date")
-margins.to_spark()
-
-# COMMAND ----------
-
-# ok, lets generate the shit using spark
-import pyspark.sql.functions as f
-import pyspark.pandas as ps
-from pyspark.mllib.random import RandomRDDs
-import datetime
-
-sc = spark.getActiveSession()
-min_date = min(dates["date"].to_numpy())-datetime.timedelta(1)
-product_base = RandomRDDs.normalVectorRDD(sc,  numRows=n_products, numCols=1, seed=seed)\
-    .map(lambda x: 0.15 + 0.15*x).map(lambda x: x.tolist()).toDF([str(min_date)])
-product_diffs = RandomRDDs.normalVectorRDD(sc, numRows=n_products, numCols=n_dates, seed=seed)\
-    .map(lambda x: 0 + 0.15*x).map(lambda x: x.tolist()).toDF([str(d) for d in dates["date"].to_numpy()])\
-
-product_base = ps.concat([ps.DataFrame(product_base),ps.DataFrame(product_diffs)], axis=1)
-product_base["product_id"] = products.product_id
-margins = ps.melt(product_base, id_vars=["product_id"], value_name="margin", var_name="date")
-margins[margins.product_id==38900088].sort_values("date").margin.cumsum()
-
-# COMMAND ----------
-
-import pyspark.pandas as ps
-ps.set_option("compute.ops_on_diff_frames", True)
-product_diffs["product_baseline"] = product_base["product_baseline"]
-
-# COMMAND ----------
-
-# RETRY ON THE REES46
-
-# USE SPARK FOR THE SIMULATION`
-
-# CONSIDER PUSHING RESULTS TO DELTA? LARGE SIMULATION TAB?
-# CONSIDER WIDE DATASET, WITH GENERATED MARGIN COLUMNS
-
-# DOCUMENT SIMULATION - DESCRIBE CALC MORE PRECISELY  
-# DOCUMENT SIMULATION - PLOT CUSTOMER MARGIN DISTRIBUTION?
-# DOCUMENT SIMULATION - PLOT ALL RANDOM WALKS / ONE-PRODUCT WALK? 
-
-# COMMAND ----------
-
-import pyspark.sql.functions as f
-import pyspark.pandas as ps
-
-dataset_name = "retailrocket"
-data_path = f"dbfs:/mnt/{dataset_name}/delta/events"
-events = spark.read.format("delta").load(data_path)
-
-
-
-purchases = events.where(f.col("event_type_name")=="purchase")\
-    .withColumn("date", f.to_date("event_time"))\
-    .groupBy(["user_id", "date"])\
-        .agg(f.sum("revenue").alias("revenue"))
-
-purchases.count()
-
-# COMMAND ----------
-
-
-
-# COMMAND ----------
-
-dates = purchases.select("date").distinct()
-users = purchases.select("user_id").distinct()
-revenue = users.crossJoin(dates).join(purchases, on=["user_id", "date"], how="left")\
-    .fillna(0, subset=["revenue"]).toPandas()
-
-# COMMAND ----------
-
-
-
-# COMMAND ----------
-
 # MAGIC %md
 # MAGIC 
 # MAGIC ### Expected campaign profit
@@ -254,42 +53,10 @@ revenue = users.crossJoin(dates).join(purchases, on=["user_id", "date"], how="le
 
 # COMMAND ----------
 
-profit_simulation_config = {"retailrocket":{
-    "gamma":{"alpha":20, "beta":113},
-    "delta":2000,
-    "psi":{"alpha":9, "beta":1},
-    "n_iter":1000,
-    "seed":1}}
+#
+##
+### SIMULATED PROFIT
 
-# COMMAND ----------
-
-def _evaluate_predictions(df, config):
-    # standard metrics
-    from sklearn.metrics import accuracy_score, precision_score
-    from sklearn.metrics import recall_score, f1_score, roc_auc_score
-    import pandas as pd
-    metrics = {m.__name__:m for m in\
-        [accuracy_score, precision_score, recall_score, f1_score, roc_auc_score]}
-    y = df["y"]
-    predicted = df["y_pred"]
-    predicted_proba = df["y_pred_proba"]
-    results ={}
-    for n,f in metrics.items():
-        if "roc_auc" in n:
-            results[n] = f(y, predicted_proba)
-        else:
-            results[n] = f(y, predicted)
-    simulated_profit = get_simulated_profit(df, config)
-    results["mep_score"] = simulated_profit.cecp.max()
-    results["map_score"] = simulated_profit.cacp[simulated_profit.cecp.idxmax()]
-    return pd.Series(results)
-
-# COMMAND ----------
-
-# NOTE: THIS MIGHT REFACTORED
-    # SIMULATE PARAMS FOR EACH WEEK
-    # JOIN SIMULATED PARAMS ON PREDICTIONS
-    # CALC THE SHIT
 def get_simulated_profit(predictions, config):
     import numpy as np
     import pandas as pd
@@ -301,7 +68,7 @@ def get_simulated_profit(predictions, config):
     
     np.random.seed(seed)
     n_users = predictions.user_id.nunique()
-    simulated_profit = []
+    sp = []
     for i in range(n_iter):
         gamma_psi = pd.DataFrame.from_dict({
             "user_id":predictions.user_id.unique(),
@@ -313,28 +80,31 @@ def get_simulated_profit(predictions, config):
         temp["acp"] = (temp["y"]*temp["gamma"]*(temp["cap"]-delta)
             + (1-temp["y"])*(-temp["psi"]*delta))
         # NOTE: OPTIMIZE DTYPES
-        simulated_profit.append(temp.loc[:,["ecp", "acp"]])
-    simulated_profit = pd.concat(simulated_profit)
-    simulated_profit = simulated_profit\
-        .sort_values("ecp", ascending=False).reset_index(drop=True)
-    simulated_profit["cecp"] = simulated_profit.ecp.cumsum()/n_iter
-    simulated_profit["perce"] = simulated_profit.ecp.rank(ascending=False, pct=True)
-    simulated_profit = simulated_profit\
-        .sort_values("acp", ascending=False).reset_index(drop=True)    
-    simulated_profit["cacp"] = simulated_profit.acp.cumsum()/n_iter
-    simulated_profit["perca"] = simulated_profit.acp.rank(ascending=False, pct=True)
-    return simulated_profit
+        sp.append(temp.loc[:,["ecp", "acp"]])
+    sp = pd.concat(sp)
+    sp = sp.sort_values("ecp", ascending=False).reset_index(drop=True)
+    sp["cecp"] = sp.ecp.cumsum()/n_iter
+    sp["perc"] = sp.ecp.rank(ascending=False, pct=True) 
+    sp["cacp"] = sp.acp.cumsum()/n_iter
+    return sp
 
-def plot_simulated_profit(simulated_profit):
+def get_campaign_profit(sp):
+    imax = sp.cecp.idxmax()
+    return {"mep_score":sp.cecp[imax],
+            "map_score":sp.cacp[imax],
+            "perc":sp.perc[imax]}
+    
+def plot_simulated_profit(sp):
+    from sklearn.preprocessing import MinMaxScaler
     import matplotlib.pyplot as plt
     import seaborn as sns
     
     f, a = plt.subplots(1,1, figsize=(15,10))
-    sns.lineplot(#data=simulated_profit,
-        x=sp.perce, y=sp.cecp, legend=False,
+    sns.lineplot(#data=sp,
+        x=sp.perc, y=sp.cecp, legend=False,
         color=sns.color_palette("rocket")[0], ax=a);
-    sns.lineplot(#data=simulated_profit,
-        x=sp.perca, y=sp.cacp, legend=False,
+    sns.lineplot(#data=sp,
+        x=sp.perc, y=sp.cacp, legend=False,
         color=sns.color_palette("rocket")[3], ax=a);
     a.set_ylabel("profit");
     a.set_xlabel("percentile");
@@ -343,9 +113,128 @@ def plot_simulated_profit(simulated_profit):
     a.axhline(0, linestyle="dotted", c="k");
     return None
 
+#
+##
+### EVALUATION
+
+def _evaluate_predictions(df, config):
+    # standard metrics
+    from sklearn.metrics import accuracy_score, precision_score
+    from sklearn.metrics import recall_score, f1_score, roc_auc_score
+    import pandas as pd
+    metrics = {m.__name__:m for m in\
+        [accuracy_score, precision_score, recall_score, f1_score, roc_auc_score]}
+    y = df["y"]
+    predicted = df["y_pred"]
+    predicted_proba = df["y_pred_proba"]
+    result_dict ={}
+    for n,f in metrics.items():
+        if "roc_auc" in n:
+            result_dict[n] = f(y, predicted_proba)
+        else:
+            result_dict[n] = f(y, predicted)
+    sp = get_simulated_profit(df, config)
+    profit_dict = get_campaign_profit(sp)
+    return pd.Series({**result_dict, **profit_dict})
+
 # COMMAND ----------
+
+#
+##
+### EVALUATE METRICS
+
+# expected values & ci bounds
+def _ci(vec, alpha=0.95):
+    import scipy.stats as st
+    import numpy as np
+    
+    mju = np.mean(vec)
+    low, hi  = st.t.interval(alpha=alpha,
+        df=len(vec)-1, loc=mju, scale=st.sem(vec))
+    return (low, mju , hi)
+
+def get_ci(df):
+    import pandas as pd
+    
+    df = df.groupby(["pipe", "type", "metric"], as_index=False)\
+        .agg(bounds=("value", _ci))
+    df = pd.concat([df, pd.DataFrame(df["bounds"].tolist(),
+        columns=["lb","mju","hb"])], axis=1)\
+            .drop("bounds",axis=1)
+    return df
+
+def _tt(df, a="value_x", b="value_y"):
+    from scipy.stats import ttest_rel
+    import numpy as np
+    tstat, pval = ttest_rel(df[a], df[b])
+    diff = np.mean(df[a]-df[b])
+    return (diff, tstat, pval)
+
+def get_tt(df):
+    import pandas as pd
+    
+    df = df.merge(df, on=["type", "week_step", "metric"])\
+        .groupby(["pipe_x","pipe_y","type","metric"])\
+            .apply(_tt).to_frame('ttest').reset_index()
+    df = pd.concat([df, pd.DataFrame(df["ttest"].tolist(),
+        columns=["diff", "stat", "pval"])], axis=1)\
+            .drop("ttest",axis=1)
+    df = df[df["pipe_x"]>df["pipe_y"]]
+    return df
+
+def plot_bv(df, metrics, figsize=(16,5)):
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    
+    tdf = pd.pivot_table(df,
+        index=["pipe","week_step", "metric"],
+            columns=["type"]).reset_index()
+    tdf.columns = ["pipe","week_step", "metric", "test", "train"]
+
+    f, axs = plt.subplots(1,3, figsize=figsize);
+    for i,m in enumerate(metrics.items()):
+        a = axs.flatten()[i]
+        sns.scatterplot(data=tdf[tdf.metric==m[0]], x="train", y="test", hue="pipe", ax=a);
+        sns.lineplot(x=[0,1],y=[0,1], color="gray", ax=a, linestyle="dotted");
+        a.set_xlim(m[1]["xlim"]);
+        #a.set_ylim(a.set_xlim());
+        a.set_xlabel(m[1]["label"]+" on training split");
+        a.set_ylabel(m[1]["label"]+" on testing split");
+        a.legend_.remove();
+    axs.flatten()[-1].legend(loc="lower right", frameon=False);
+
+# COMMAND ----------
+
+profit_simulation_config = {"retailrocket":{
+    "gamma":{"alpha":22.3, "beta":200},
+    #"gamma":{"alpha":20.5, "beta":113},
+    "delta":800,
+    "psi":{"alpha":9, "beta":1},
+    "n_iter":100,
+    "seed":1}}
 
 dataset_name = "retailrocket"
 predictions = spark.table(f"churndb.{dataset_name}_predictions").toPandas()
-predictions.groupby(["pipe", "type", "week_step"], as_index=False)\
-    .apply(_evaluate_predictions, profit_simulation_config[dataset_name])
+evaluation = predictions.groupby(["pipe", "type", "week_step"], as_index=False)\
+    .apply(_evaluate_predictions, profit_simulation_config["retailrocket"])
+
+melted_evaluation = evaluation.melt(id_vars=["type","week_step", "pipe"],
+    var_name="metric", value_name="value")
+
+display(get_ci(melted_evaluation).fillna(0))
+display(get_tt(melted_evaluation))
+
+metrics = {"accuracy_score":{"label":"acc", "xlim":(0,1)},
+    "f1_score":{"label":"f1", "xlim":(0,1)},
+    "roc_auc_score":{"label":"auc", "xlim":(0,1)}}    
+plot_bv(melted_evaluation, metrics=metrics)  
+
+# COMMAND ----------
+
+
+dataset_name = "retailrocket"
+predictions = spark.table(f"churndb.{dataset_name}_predictions").toPandas()
+predictions = predictions[((predictions.week_step==1))&(predictions.type=="test")&(predictions["pipe"]=="hgb")]
+sp = get_simulated_profit(predictions, profit_simulation_config["retailrocket"])
+plot_simulated_profit(sp)
