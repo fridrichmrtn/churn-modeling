26d44319a6494b62e2e0da25848e522cfd3c963b
fridrich.mrtn@gmail.com <fridrich.mrtn@gmail.com>
2022-07-07 15:47:04 +0000

    Test run

--- code/modeling/run-modeling.py
+++ code/modeling/run-modeling.py
@@ -7,5 +7,5 @@
 
 # COMMAND ----------
 
-spark.sql("DELETE FROM churndb.retailrocket_predictions WHERE pipe='mlp' AND week_step=1")
+spark.sql("DELETE FROM churndb.retailrocket_predictions")
 glue_pipeline("retailrocket", range(1,2), False)
