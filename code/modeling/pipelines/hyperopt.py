da9dedbb402221c2b03f7ef3f56f97faadbfa63f
fridrich.mrtn@gmail.com <fridrich.mrtn@gmail.com>
2022-07-08 10:35:13 +0000

    Add simple multi-output support

--- code/modeling/pipelines/hyperopt.py
+++ code/modeling/pipelines/hyperopt.py
@@ -32,19 +32,23 @@ def _get_exp_id(exp_path):
 def _train_test_dict(X, y, test_size, seed):
     from sklearn.model_selection import train_test_split
     
+    if len(y.shape)>1:
+        y_strata = y[:,0]
+        
     X_train, X_test, y_train, y_test = train_test_split(X, y,
-        test_size=test_size, stratify=y, random_state=seed) 
+        test_size=test_size, stratify=y_strata, random_state=seed) 
     return {"train":{"X":X_train, "y":y_train},
         "test":{"X":X_test, "y":y_test}}
-
-def _get_performance(model, X, y):
-    from sklearn.metrics import accuracy_score, precision_score
-    from sklearn.metrics import recall_score, f1_score, roc_auc_score
     
+def _get_class_perf(model, X, y):
+    from sklearn.metrics import (accuracy_score, precision_score,
+        recall_score, f1_score, roc_auc_score)
+
     metrics = {m.__name__:m for m in\
         [accuracy_score, precision_score, recall_score, f1_score, roc_auc_score]}
     predicted = model.predict(X)
-    
+    if len(y.shape)>1:
+        y = y[:,0]    
     results ={}
     for n,f in metrics.items():
         if "roc_auc" in n:
@@ -57,23 +61,47 @@ def _get_performance(model, X, y):
             results[n] = f(y, predicted)
     return results
 
+def _get_reg_perf(model, X, y):
+    from sklearn.metrics import r2_score, mean_squared_error
+    
+    metrics = {m.__name__:m for m in\
+            [r2_score, mean_squared_error]}
+    if len(y.shape)>1:
+        y = y[:,1]
+        predicted = model.predict(X, scope="regression")
+        results = {n:f(y, predicted) for n,f in metrics.items()}
+    else:
+        results ={n:None for n,f in metrics.items()}
+    return results
+
+def _get_performance(model, X, y):
+    class_metrics = _get_class_perf(model, X, y)
+    reg_metrics = _get_reg_perf(model, X, y)
+    return dict(class_metrics, **reg_metrics)
+
 def _evaluate_hyperopt(params, pipe, X, y, seed):
     from hyperopt import STATUS_OK
     from sklearn.model_selection import train_test_split
+    from scipy.stats import hmean
   
     data_dict = _train_test_dict(X, y, .4, seed)
     pipe.set_params(**params)
     pipe.fit(data_dict["train"]["X"], data_dict["train"]["y"])
     metrics = {n+"_"+m:v for n, data in data_dict.items()
         for m,v in _get_performance(pipe, data["X"], data["y"]).items()}
-    return {"loss":-metrics["test_f1_score"], "status":STATUS_OK}
+    
+    if len(y.shape)>1:
+        loss = hmean([metrics["test_f1_score"], metrics["test_r2_score"]])
+    else:
+        loss = metrics["test_f1_score"]
+    return {"loss":-loss, "status":STATUS_OK}
 
 
 def _optimize_pipeline(data, pipe):
     import mlflow
     from hyperopt import fmin, space_eval
     from functools import partial
-    
+        
     # set the experiment logging
     X, y = _get_Xy(data, pipe)
     exp_name = "{}_{}_hyperopt".format(data["name"],pipe["name"])
