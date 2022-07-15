58afc8380aa720a707e2a62c13981985c927a463
fridrich.mrtn@gmail.com <fridrich.mrtn@gmail.com>
2022-07-15 16:01:59 +0000

    Modify column selection func to accommodate both classification/regression tasks

--- code/modeling/pipelines/workhorse.py
+++ code/modeling/pipelines/workhorse.py
@@ -25,30 +25,34 @@ from sklearn.model_selection import train_test_split
 # COMMAND ----------
 
 def _get_cols(train):
-    out_multi_cols = ["user_id", "row_id", "target_event",
-        "target_revenue", "week_step", "target_cap"]
-    out_standard_cols = ["cap", "cap_month_lag0", "cap_month_lag1",
-        "cap_month_lag2", "cap_month_lag3", "cap_month_ma3"]
-    multi_cols = [c for c in train.columns if c not in set(out_multi_cols)]
-    standard_cols = [c for c in multi_cols if c not in set(out_standard_cols)]
-    return (standard_cols, multi_cols)
+    class_y = ["target_event"]
+    reg_y = ["target_actual_profit"]
+    target_cols = [c for c in train.columns if "target_" in c]
+    helper_cols = ["user_id", "row_id", "week_step"] # NOTE: week_step?
+    cust_val_cols = [c for c in train.columns if "customer_value" in c]
+    class_set = set(target_cols+helper_cols+cust_val_cols)
+    reg_set = set(target_cols+helper_cols)
+    class_X = [c for c in train.columns if c not in class_set]
+    reg_X = [c for c in train.columns if c not in reg_set]
+    return {"classification":{"X":class_X,"y":class_y},
+        "regression":{"X":reg_X,"y":reg_y}}
 
 def _get_dataset(dataset_name, week_step):
     data = spark.table(f"churndb.{dataset_name}_customer_model")\
         .where(f.col("week_step")>=week_step).toPandas()
     train = data[data.week_step>week_step]
     test = data[data.week_step==week_step]			
-    standard_cols, multi_cols = _get_cols(train)
+    columns = _get_cols(train)
     return {
       "train":
             {"raw":optimize_numeric_dtypes(train),
              "week_step":week_step,
-             "columns":{"standard":standard_cols, "multi-output":multi_cols},
+             "columns":columns,
              "name":f"{dataset_name}_{week_step}"},
       "test":
             {"raw":optimize_numeric_dtypes(test),
              "week_step":week_step,
-             "columns":{"standard":standard_cols, "multi-output":multi_cols},
+             "columns":columns,
              "name":f"{dataset_name}_{week_step}"}}      
     
 def _fit_calibrated_pipeline(data, pipe):
