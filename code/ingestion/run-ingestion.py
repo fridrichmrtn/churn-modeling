11e3e2e222eac5ee7153b3ae3f558d90bd376fbf
fridrich.mrtn@gmail.com <fridrich.mrtn@gmail.com>
2022-09-13 16:35:18 +0000

    Re-run

--- code/ingestion/run-ingestion.py
+++ code/ingestion/run-ingestion.py
@@ -24,10 +24,10 @@
 
 # CUSTOMER MODEL
 #_prerun_optimize_recom(dataset_name)
-# customer_model = construct_customer_model(
-#      dataset_name, time_steps=4, week_target=4)
-# save_customer_model(
-#      customer_model, dataset_name, overwrite=True)
+customer_model = construct_customer_model(
+     dataset_name, time_steps=4, week_target=4)
+save_customer_model(
+     customer_model, dataset_name, overwrite=True)
 
 # COMMAND ----------
 
@@ -35,7 +35,7 @@
 #
 ## REES46
 
-dataset_name = "rees46"
+#dataset_name = "rees46"
  
 # LOAD TRANSFORM
 #events = construct_events(dataset_name)
@@ -43,12 +43,14 @@ dataset_name = "rees46"
  
 # CUSTOMER MODEL
 #_prerun_optimize_recom(dataset_name)
-customer_model = construct_customer_model(
-    dataset_name, time_steps=7, week_target=4)
-save_customer_model(customer_model,
-    dataset_name, overwrite=True)
+# customer_model = construct_customer_model(
+#     dataset_name, time_steps=7, week_target=4)
+# save_customer_model(customer_model,
+#     dataset_name, overwrite=True)
 
 # COMMAND ----------
 
-spark.table("churndb.rees46_customer_model").write.format("parquet")\
-    .mode("overwrite").save("dbfs:/mnt/rees46/raw/customer_model/")
+spark.table("churndb.retailrocket_customer_model").write.format("parquet")\
+    .mode("overwrite").save("dbfs:/mnt/retailrocket/raw/customer_model/")
+#spark.table("churndb.rees46_customer_model").write.format("parquet")\
+#    .mode("overwrite").save("dbfs:/mnt/rees46/raw/customer_model/")
